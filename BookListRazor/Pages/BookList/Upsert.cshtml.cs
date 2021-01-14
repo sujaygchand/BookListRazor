60962aac10a11da2e8fca49b3f086eebf84ca97c
Sujay Chand <sujaygchand@gmail.com>
2021-01-14 16:10:31 +1300

    Combined edit and create into an upsert page

---
+++ BookListRazor/Pages/BookList/Upsert.cshtml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookListRazor.Pages.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookListRazor
+{
+    public class UpsertModel : PageModel
+    {
+		private ApplicationDbContext _db;
+
+		public UpsertModel(ApplicationDbContext db)
+		{
+			_db = db;
+		}
+
+		[BindProperty]
+		public Book Book { get; set; }
+
+		public async Task<IActionResult> OnGet(int? id = null)
+		{
+			Book = new Book();
+
+			if (id == null)
+				return Page();
+
+			Book = await _db.Book.FirstOrDefaultAsync(u => u.Id == id);
+
+			if (Book == null)
+				return NotFound();
+
+			return Page();
+		}
+
+		public async Task<IActionResult> OnPost()
+		{
+			if (ModelState.IsValid == false)
+				return RedirectToPage();
+
+			if(Book.Id == 0)
+			{
+				_db.Book.Add(Book);
+			}
+			else
+			{
+				_db.Book.Update(Book);
+			}
+
+			await _db.SaveChangesAsync();
+
+			return RedirectToPage("Index");
+		}
+	}
+}
--- BookListRazor/Pages/Model/Book.cs
+++ BookListRazor/Pages/Model/Book.cs
@@ -15,5 +15,6 @@ namespace BookListRazor.Pages.Model
 		public string Name { get; set; }
 		public string Author { get; set; }
 
+		public string ISBN { get; set; }
 	}
 }
